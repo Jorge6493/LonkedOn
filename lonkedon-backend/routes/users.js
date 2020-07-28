2125bb2df842d424c9af30dfd855737ab7ef2720
Pedro Guillermo Rodríguez <20155001@ce.pucmm.edu.do>
2020-07-27 21:59:43 -0400

    Get and Post Users

--- lonkedon-backend/routes/users.js
+++ lonkedon-backend/routes/users.js
@@ -3,11 +3,16 @@ var router = express.Router();
 
 var User = require("../models/user_model").User;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a GET for Pedro');
+// GET ALL USERS
+router.get('/', function (req, res, next) {
+	User.find().exec(function (err, usuarios) {
+		if (err)
+			console.log(err);
+		res.send(usuarios);
+	});
 });
 
+// POST USER
 router.post("/create", function (req, res,next) {
 
 	var user = new User({
