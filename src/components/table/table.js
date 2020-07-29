747d11de2ec2457e83f34b2f79b2e8d4e1ea156d
Franz Pichardo Rodriguez <franz.pichardo.r@gmail.com>
2020-07-29 15:16:54 -0400

    Update table.js

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -5,7 +5,6 @@ import './table.css';
 // import Jobs from '../jobs/jobs';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import { render } from 'react-dom';
 // import { MTableToolbar } from 'material-table';
 
 const jobs = [
@@ -26,57 +25,57 @@ class Table extends Component {
   state = {
     loading: true,
     data: {},
-    dataAvail:null
+    dataAvail: null
   }
 
-//   async componentDidMount() {
-//     // GET request using axios with async/await
+  //   async componentDidMount() {
+  //     // GET request using axios with async/await
 
-//     const response = await axios.get('http://localhost:3500/jobs');
-//     this.setState({ data: response.data })
-//     console.log('response')
-//     console.log(response.data)
-// }
+  //     const response = await axios.get('http://localhost:3500/jobs');
+  //     this.setState({ data: response.data })
+  //     console.log('response')
+  //     console.log(response.data)
+  // }
 
-componentDidMount() {
-  // Simple GET request using axios
-  axios.get('http://localhost:3500/jobs')
-      .then(response => this.setState({ data: response.data, dataAvail:true }));
-}
-  
+  componentDidMount() {
+    // Simple GET request using axios
+    axios.get('http://localhost:3500/jobs')
+      .then(response => this.setState({ data: response.data, dataAvail: true }));
+  }
+
   render() {
     // console.log('ta entrando')
     // console.log(this.state.data)
-    
+
     const dataAvail = this.state.dataAvail;
     let table;
     if (dataAvail) {
       table = <MaterialTable
-      options={{pageSize: parseInt(this.props.size),search: true }}
+        options={{ pageSize: parseInt(this.props.size), search: true }}
         columns={[
           { title: 'Location', field: 'location' },
           { title: 'Position', field: 'position' },
           { title: 'Category', field: 'category' }
         ]}
-      
-       data={this.state.data}
-    
+
+        data={this.state.data}
+
         title=''
         components={{
           Toolbar: props => (<div>
             <MTableToolbar {...props} />
-              <div style={{padding: '0px 10px'}}>
+            <div style={{ padding: '0px 10px' }}>
               <h5>
-                  <Link to={'/jobs/'+ this.props.jobId }>{jobs[this.props.jobId-1].title}'s Table</Link>
-                  </h5>
-              </div>
+                <Link to={'/jobs/' + this.props.jobId}>{jobs[this.props.jobId - 1].title}'s Table</Link>
+              </h5>
+            </div>
           </div>
-         
+
           )
-      }}
-         
+        }}
+
       />;
-    } 
+    }
 
     return (
       <div style={{ maxWidth: '100%' }}>
