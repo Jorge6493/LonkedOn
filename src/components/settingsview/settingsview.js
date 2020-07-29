6244f95c4d8a28224f5c4a3f7c4158394d1cd407
Franz Pichardo Rodriguez <franz.pichardo.r@gmail.com>
2020-07-29 15:23:57 -0400

    Added Settings View

---
+++ src/components/settingsview/settingsview.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './settingsview.css';
+import HomeButton from "../homebutton/homebutton";
+import Logout from "../logout";
+import { Link } from "react-router-dom";
+
+export default class SettingsView extends React.Component {
+    render() {
+        return(
+            <div class="container">
+                <div class="row float-right">
+                    <Logout/>
+                </div>
+                <h1 class="ml-5 pt-4">Bolsa de Empleos "Tigre de Web"</h1>
+                <br/>
+                <div class="input-group col-sm-6">
+                    <HomeButton />
+                </div>
+                <br/>
+                <Link to="/editcat">
+                    <h3 class="col-sm-6">Edit Categories</h3>
+                </Link>                
+                <br/>
+                <Link to="/editjobs">
+                    <h3 class="col-sm-6">Edit Job Postings</h3>
+                </Link>
+                
+            </div>
+        );
+    }
+}
