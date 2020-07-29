7b77b86fb0f4ea102280708b1fc7bb92ff204d0f
Franz Pichardo Rodriguez <franz.pichardo.r@gmail.com>
2020-07-29 16:08:21 -0400

    Login goes to home

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,6 +4,7 @@ import Settings from "../settings";
 import Postjob from "../postjob/postjob";
 import HomeButton from "../homebutton/homebutton";
 import axios from "axios";
+import Home from "../home";
 
 export default class Login extends React.Component {
 
@@ -11,18 +12,34 @@ export default class Login extends React.Component {
         super(props)
         this.state = {
             username: "",
-            password: ""
+            password: "",
+            valid: false
         }
     }
 
     postContent() {
-        axios.post("http://localhost:3500/users", {
+        axios.post("http://localhost:3500/users/login", {
             username: this.state.username,
             password: this.state.password
-        }).then(this.setState({
-            username: "",
-            password: ""
-        }))
+        }).then((response) => {
+            if (response.data === "") {
+                alert("Incorrect Username or Password");
+                this.setState({
+                    username: "",
+                    password: "",
+                    valid: false
+                })
+            }
+
+            else {
+                this.user_id = response.data;
+                this.setState({
+                    username: "",
+                    password: "",
+                    valid: true
+                })
+            }
+        })
     }
     
     handleChange = (event, fieldName) => {
@@ -30,31 +47,39 @@ export default class Login extends React.Component {
     };
 
     render() {
-        return (
-            <div class="container">    
-                <div class="row float-right">
-                    <Settings />
-                </div>
-                <h1 class="ml-5 pt-4">Bolsa de Empleos "Tigre de Web"</h1>
-                <br />
-                <div class="row">
-                    <div class="input-group col-sm-6">
-                        <HomeButton />
+        if (!this.state.valid){
+            return (
+                <div class="container">
+                    <div class="row float-right">
+                        <Settings />
                     </div>
-                    <div class="input-group col-sm-2 ml-auto">
+                    <h1 class="ml-5 pt-4">Bolsa de Empleos "Tigre de Web"</h1>
+                    <br />
+                    <div class="row">
+                        <div class="input-group col-sm-6">
+                            <HomeButton />
+                        </div>
+                        <div class="input-group col-sm-2 ml-auto">
                         <Postjob />
+                        </div>
                     </div>
+                    <div class="col-md-6 center">
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            <input type="text" class="form-control" placeholder="Enter username" id="username" value={this.state.username} onChange={event => this.handleChange(event, "username")} />
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" placeholder="Enter password" id="password" value={this.state.password} onChange={event => this.handleChange(event, "password")} />
+                        </div>
+                    </div>
+                    <button onClick={this.postContent.bind(this)}>Submit</button>
                 </div>
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" class="form-control" placeholder="Enter username" id="username" value={this.state.username} onChange={event => this.handleChange(event, "username")}/>
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password"  class="form-control"placeholder="Enter password" id="password" value={this.state.password} onChange={event => this.handleChange(event, "password")}/>
-                </div>
-                <button onClick={this.postContent.bind(this)}>Submit</button>
-            </div>    
-        );
+            );
+        } else {
+            return (
+                <Home/>
+            );
+        }
     }
 }
